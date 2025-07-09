e36ca982e858a6dc92b86002ab4e3dcff83179ef
Mohit Prajapat <mohitprajapat242004@gmail.com>
2025-07-09 12:54:12 +0530

    09-07-2025

--- spring-boot-simple/src/main/java/com/rays/ctl/LoginCtl.java
+++ spring-boot-simple/src/main/java/com/rays/ctl/LoginCtl.java
@@ -1,9 +1,11 @@
 package com.rays.ctl;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rays.common.BaseCtl;
 import com.rays.common.ORSResponse;
+import com.rays.config.JWTUtil;
 import com.rays.dto.UserDTO;
 import com.rays.form.LoginForm;
 import com.rays.form.UserRegistrationForm;
@@ -21,10 +24,13 @@ import com.rays.service.UserService;
 public class LoginCtl extends BaseCtl {
 
 	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
 	public UserService service;
 
 	@PostMapping("login")
-	public ORSResponse login(@RequestBody @Valid LoginForm form, BindingResult bindingResult) {
+	public ORSResponse login(@RequestBody @Valid LoginForm form, BindingResult bindingResult, HttpSession session) {
 
 		ORSResponse res = validate(bindingResult);
 
@@ -34,10 +40,25 @@ public class LoginCtl extends BaseCtl {
 
 		UserDTO dto = service.authenticate(form.getLoginId(), form.getPassword());
 		if (dto != null) {
+			String token = jwtUtil.generateToken(form.getLoginId());
+			session.setAttribute("user", dto);
 			res.addData(dto);
+			res.addResult("token", token);
 		} else {
 			res.addMessage("Login ID & Password is invalid..!!");
 		}
+		return res;
+	}
+	
+	@GetMapping("logout")
+	public ORSResponse logout(HttpSession session) {
+
+		ORSResponse res = new ORSResponse();
+
+		session.invalidate();
+
+		res.addMessage("Logout successfully..!!");
+
 		return res;
 	}
 
